ed018c110f34bac54c777fa4aae0f55fccaa57d8
ActiveZ <activez.git@gmail.com>
2021-03-08 12:09:55 +0100

    Ajoutez des fichiers projet.

---
+++ DemoCnam/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DemoCnam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int essai, cpt = 0;
+            Random rnd = new Random();
+            
+            int x = rnd.Next(1, 20);
+
+            do
+            {
+                cpt++;
+                Console.Write("Entrer votre essai entre 1 et 20: ");
+                essai = int.Parse(Console.ReadLine());
+                if (essai < x)  Console.WriteLine("trop petit");
+                if (essai > x) Console.WriteLine ("trop grand");
+            } 
+            while (essai != x);
+            Console.WriteLine("Gagné en "+ cpt + " coups !!!");
+        }
+    }
+}
